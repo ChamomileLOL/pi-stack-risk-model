ee744de73225f861edea7194c9c04b9deb51847c
Xavier Moorkattil <xavier.siby777@gmail.com>
2025-12-08 19:49:26 +0530

    fix: set vite base path for vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+﻿import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  // 🏆 FIX: Tell Vercel to look for assets starting from the root URL.
+  base: '/', 
   plugins: [solid()],
-})
+});
